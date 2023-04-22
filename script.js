bbdba59ff13fcea6c7c34d4731f36f019f382644
Monster <untilov.2a@gmail.com>
2023-04-22 13:11:42 +0300

    fixed timer

--- script.js
+++ script.js
@@ -20,6 +20,15 @@ function togglePlay() {
 
 function getTimeRemaining(endtime) {
   var t = Date.parse(endtime) - Date.parse(new Date());
+  if (Date.parse(endtime) < Date.parse(new Date())) {
+    return {
+      'total': 0,
+      'days': 0,
+      'hours': 0,
+      'minutes': 0,
+      'seconds': 0
+    };
+  }
   var seconds = Math.floor((t / 1000) % 60);
   var minutes = Math.floor((t / 1000 / 60) % 60);
   var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
