4bef7d4670a55057d77f1a937f25d1a4f8ccc89a
Monster <untilov.2a@gmail.com>
2024-04-27 17:56:16 +0300

    New era of dnd foundry vtt

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline="October 22 2023 13:00:00 GMT+0300";
+var deadline="June 2 2024 13:00:00 GMT+0300";
 initializeClock('countdown', deadline);
 
 function magnify(imgID, zoom) {
